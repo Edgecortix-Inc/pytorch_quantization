d1eb1f6636760360ded1b5cc3b429c52457a8202
Masahiro Masuda <masahi129@gmail.com>
2020-01-25 12:05:42 +0900

    refactor parse_ops

--- torch_frontend_test.py
+++ torch_frontend_test.py
@@ -845,34 +845,33 @@ def parse_params(script_module, input_vars):
     return params, param_tensors
 
 
+def get_input_types(op_node):
+    input_list_types = []
+    for input_node in op_node.inputs():
+        in_ty = input_node.type()
+        input_node_kind = in_ty.kind()
+        if input_node_kind == 'TensorType':
+            if in_ty.scalarType() is None:
+                input_list_types.append('float')
+            else:
+                input_list_types.append(in_ty.scalarType().lower())
+        elif input_node_kind == 'ListType':
+            input_list_types.append(str(in_ty.getElementType()).lower())
+        elif input_node_kind in ['IntType', 'FloatType', 'BoolType', 'StringType', 'OptionalType']:
+            input_list_types.append(str(in_ty).lower())
+        else:
+            input_list_types.append('UnsupportedType')
+
+    node_type = op_node.output().type()
+    if op_node.kind() in ['aten::ones', 'aten::zeros']:
+        input_list_types[0] = node_type.scalarType().lower()
+
+    return input_list_types
+
+
 def parse_ops(script_module, input_vars):
     ops = {}
     op_inputs_types = {}
-
-    def add_op(node_id, op_node):
-        ops[node_id] = op_node
-        input_list_types = []
-        for input_node in op_node.inputs():
-            in_ty = input_node.type()
-            input_node_kind = in_ty.kind()
-            if input_node_kind == 'TensorType':
-                if in_ty.scalarType() is None:
-                    input_list_types.append('float')
-                else:
-                    input_list_types.append(in_ty.scalarType().lower())
-            elif input_node_kind == 'ListType':
-                input_list_types.append(str(in_ty.getElementType()).lower())
-            elif input_node_kind in ['IntType', 'FloatType', 'BoolType', 'StringType', 'OptionalType']:
-                input_list_types.append(str(in_ty).lower())
-            else:
-                input_list_types.append('UnsupportedType')
-
-        node_type = op_node.output().type()
-        if op_node.kind() in ['aten::ones', 'aten::zeros']:
-            input_list_types[0] = node_type.scalarType().lower()
-
-        op_inputs_types[node_id] = input_list_types
-
     consts = {}
     list_input_vars = {}
     # Traverse nodes and add to graph
@@ -909,20 +908,21 @@ def parse_ops(script_module, input_vars):
             list_input_vars[node_name] = _expr.var(node_name, shape=list_shape)
 
         if node.kind() != "prim::GetAttr":
-            add_op(node_name, node)
+            ops[node_name] = node
+            op_inputs_types[node_name] = get_input_types(node)
 
     return consts, ops, op_inputs_types, list_input_vars
 
 
 def parse_script_module(script_module, input_shapes):
     input_vars = parse_inputs(script_module, input_shapes)
-    params, param_tensors = parse_params(script_module, input_vars)
-    consts, ops, op_inputs_types, list_inputs_vars = parse_ops(script_module, input_vars)
-    input_vars.update(list_inputs_vars)
+    param_vars, param_tensors = parse_params(script_module, input_vars)
+    consts, ops, op_in_types, list_vars = parse_ops(script_module, input_vars)
+    input_vars.update(list_vars)
 
     outputs = []
     node_name_to_nid = {}
-    for k, v in {**input_vars, **params}.items():
+    for k, v in {**input_vars, **param_vars}.items():
         node_name_to_nid[k] = len(outputs)
         outputs.append(v)
 
@@ -942,7 +942,7 @@ def parse_script_module(script_module, input_shapes):
                 op_inputs.append(outputs[inode_name])
 
             call = convert_map[operator](op_inputs,
-                                         op_inputs_types[node_name])
+                                         op_in_types[node_name])
             outputs.append(call)
 
     body = outputs[-1]
