5e2f30a501a369aca4fbe4663bfd49890c1ac56d
masahi <masahi129@gmail.com>
2020-02-04 14:39:28 +0900

    add quant/dequant only test

--- frontend/qnn_test.py
+++ frontend/qnn_test.py
@@ -24,21 +24,11 @@ def quantize_model(model, inp, per_channel=False):
     torch.quantization.convert(model, inplace=True)
 
 
-class ConvBNReLU(nn.Sequential):
-    def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, groups=1):
-        padding = (kernel_size - 1) // 2
-        super().__init__(
-            nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=False),
-            nn.BatchNorm2d(out_planes, momentum=0.1),
-            # Replace with ReLU
-            nn.ReLU(inplace=False)
-        )
-
 class AnnotatedConvModel(torch.nn.Module):
     def __init__(self):
         super(AnnotatedConvModel, self).__init__()
         self.qconfig = default_qconfig
-        self.conv = torch.nn.Conv2d(3, 3, 3, bias=False).to(dtype=torch.float)
+        self.conv = torch.nn.Conv2d(3, 16, 3, padding=1, bias=False).to(dtype=torch.float)
         self.quant = QuantStub()
         self.dequant = DeQuantStub()
 
@@ -52,32 +42,47 @@ class AnnotatedConvModel(torch.nn.Module):
         pass
 
 
-inp = torch.rand(1, 3, 224, 224, dtype=torch.float)
+class QuantizeDequantize(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        quant = torch.quantize_per_tensor(x, 0.0078, 0, torch.quint8)
+        dequant = torch.dequantize(quant)
+        return dequant
+
+    def fuse_model(self):
+        pass
+
+
+def nhwc_to_nchw(out):
+    nchw_out = np.zeros(out.shape)
+    N, C, H, W = out.shape
+    inp = out.flatten()
+    for n in range(N):
+        for c in range(C):
+            for h in range(H):
+                for w in range(W):
+                    nchw_out[n][c][h][w] = inp[c + w * C + h * C * W + n * C * W * H]
+    return nchw_out
+
+
+inp = torch.abs(torch.rand(1, 3, 224, 224, dtype=torch.float))
+tvm_inp = inp.numpy().copy()
 input_name = 'X'
 input_shapes = {input_name: (1, 3, 224, 224)}
 
 qmodels = [
     AnnotatedConvModel().eval()
+    # QuantizeDequantize().eval()
     # qresnet.resnet18(pretrained=True).eval()
 ]
 
-fpmodels = [
-    ConvBNReLU(3, 3).eval()
-]
-
-quant = True
-models = fpmodels
-if quant:
-    models = qmodels
-
-for raw_model in models:
-    if quant:
-        quantize_model(raw_model, inp)
+for raw_model in qmodels:
+    quantize_model(raw_model, inp)
     script_module = torch.jit.trace(raw_model, inp).eval()
+    # script_module = torch.jit.script(raw_model)
     mod, params = parse_script_module(script_module, input_shapes)
-    print(params.keys())
-    torch._C._jit_pass_inline(script_module.graph)
-    print(script_module.graph)
 
     with torch.no_grad():
         pt_result = script_module(inp).numpy()
@@ -90,8 +95,26 @@ for raw_model in models:
 
     runtime = tvm.contrib.graph_runtime.create(json, lib, tvm.context("cpu", 0))
     runtime.set_input(**params)
-    runtime.set_input("X", inp.numpy())
+    runtime.set_input("X", tvm_inp)
     runtime.run()
     tvm_result = runtime.get_output(0).asnumpy()
     np.allclose(tvm_result, pt_result)
-    print(np.max(np.abs(tvm_result - pt_result)), np.mean(np.abs(tvm_result - pt_result)))
+    print("tvm vs torch max and mean abs diff:", np.max(np.abs(tvm_result - pt_result)), np.mean(np.abs(tvm_result - pt_result)))
+    # print(tvm_result)
+
+# from qnn_torch import *
+
+# graph = script_module.graph
+# torch._C._jit_pass_inline(graph)
+# print(graph)
+# for name, mod in script_module.named_modules():
+#     print(name, mod)
+
+# state_dict = script_module.state_dict()
+# params = get_weight_quant_params(state_dict)
+# qparam = params["conv._packed_params"]
+# print(script_module.conv.scale, script_module.conv.zero_point)
+
+# print(state_dict["quant.scale"])
+# print(qparam.scale, qparam.zero_point)
+# print(state_dict["quant.scale"].numpy() * qparam.scale.data.asnumpy())
