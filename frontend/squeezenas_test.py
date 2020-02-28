aeffa70b8ac8ce11933955fd2c300208d12e0f53
masahi <masahi129@gmail.com>
2020-02-28 17:02:01 +0900

    Squashed commit of the following:
    commit b48e04706c53fdbf476ef64535d8cba736567038
    Author: masahi <masahi129@gmail.com>
    Date:   Fri Feb 28 16:58:12 2020 +0900
    
        add readme
    
    commit 071a197747d1df1132171ca5e0fd7aa407d717dd
    Author: masahi <masahi129@gmail.com>
    Date:   Fri Feb 28 16:44:18 2020 +0900
    
        add benchmark
    
    commit abb468561fbab4b0431c419faa95c60428771141
    Author: masahi <masahi129@gmail.com>
    Date:   Wed Feb 26 12:44:01 2020 +0900
    
        update squeezenas test script to do full calibration and evaluation
    
    commit 1feb8c46c4ea940ddbfc7c08fe87bc547cb3a594
    Author: masahi <masahi129@gmail.com>
    Date:   Wed Feb 26 11:06:51 2020 +0900
    
        piggy back to fp32 for upsampling
    
    commit 1304e9ea7cf161595aa5ece2d392021381fd0ce3
    Author: masahi <masahi129@gmail.com>
    Date:   Fri Feb 14 17:59:24 2020 +0900
    
        add upsampling test

--- frontend/squeezenas_test.py
+++ frontend/squeezenas_test.py
@@ -6,6 +6,14 @@ from tvm import relay
 
 from torch_frontend import parse_script_module
 
+
+def benchmark_tvm(mod, ctx):
+    num_repeat = 1000
+    ftimer = mod.module.time_evaluator("run", ctx, number=1, repeat=num_repeat)
+    prof_res = np.array(ftimer().results)
+    print("TVM FPS:", 1.0/np.mean(prof_res))
+
+
 input_name = 'X'
 input_shapes = {input_name: (1, 3, 1024, 2048)}
 inp = torch.rand(input_shapes[input_name], dtype=torch.float)
@@ -18,6 +26,8 @@ if not os.path.exists("data"):
     print("So that the path pytorch_quantization/frontend/data/ exists")
     exit(1)
 
+quantize = True
+
 if os.path.exists("../../squeezenas/"):
     import sys
     sys.path.append("../../squeezenas")
@@ -29,7 +39,8 @@ if os.path.exists("../../squeezenas/"):
     params = torch.load(model_file, map_location=torch.device('cpu'))
     model.load_state_dict(params)
 
-    quantize_model(model, num_calib_samples=50)
+    if quantize:
+        quantize_model(model, num_calib_samples=50)
 
     script_module = torch.jit.trace(model, inp)
     mod, params = parse_script_module(script_module, input_shapes)
@@ -40,7 +51,8 @@ if os.path.exists("../../squeezenas/"):
     with relay.build_config(opt_level=3):
         json, lib, params = relay.build(mod, target="cuda", params=params)
 
-    runtime = tvm.contrib.graph_runtime.create(json, lib, tvm.context("gpu", 0))
+    ctx = tvm.context("gpu", 0)
+    runtime = tvm.contrib.graph_runtime.create(json, lib, ctx)
     runtime.set_input(**params)
     runtime.set_input(input_name, inp.numpy())
     runtime.run()
@@ -48,6 +60,8 @@ if os.path.exists("../../squeezenas/"):
     np.allclose(tvm_result, pt_result)
     abs_diff = np.abs(tvm_result - pt_result)
     print(np.max(abs_diff), np.mean(np.abs(tvm_result - pt_result)))
+
+    benchmark_tvm(runtime, ctx)
 
     def model_func(torch_inp):
         tvm_inp = torch_inp.numpy()
